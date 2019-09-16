d8747e8b159b4aa55e6db630e00ab48cde0f66a1
Victoria Duraes <victoria@janusproj.com>
2019-09-16 10:45:19 -0300

    Change consumer address

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,7 +1,7 @@
 module.exports = {
-    ADDRESS: "asdasd",
+    ADDRESS: '0xd7BC6e6e8155D3a7Dae453705235DF96B8d7B99c',
     provider: 'http',
     host: 'rinkeby.caralabs.me',
     port: 18575,
-    primaryKey: '0x18BE43587D0426247FB9ECB5F71734FAB03EA8418869F68599712A145D49ACC7'
+    primaryKey: '0xafcde7e4ee8c0de0f7fe27c642b1e4134e5a2cd2152f085a1fccdf32eee43f39'
 }
