a46448b13cbc9b7f52d5f9abbcf4c04fece05ce8
Paulo <paulotarso@janusproj.com>
2019-11-21 11:42:25 -0300

    Node_config_tag_v1-2

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-    ADDRESS: '0xd7BC6e6e8155D3a7Dae453705235DF96B8d7B99c',
+    ADDRESS: '0x76dE4BE2c318ab20b2c23dE2674184E95e360a8A',
     provider: 'https',
     host: 'rinkeby.infura.io/v3/34cfe59bd42449e78d2fea09accea995',
     port: 0,
